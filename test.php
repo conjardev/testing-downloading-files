4547cc6825ea200e2ad725b2537fc87590e84082
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2022-02-07 21:11:59 +0000

    Update test.php

--- test.php
+++ test.php
@@ -10,13 +10,6 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
-} else {
-  echo "Error creating database: " . $conn->error;
-}
 
 $conn->close();
 ?>
