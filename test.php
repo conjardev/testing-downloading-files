7c3fec1e141d7fc80119e35ac3a8ee4bc09360f0
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2022-02-07 20:48:05 +0000

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost:3306";
 $username = "root";
-$password = "";
+$password = "MyNewPass";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
