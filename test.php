7a68c23bb10f3144a8c51b0f5b4b556855242db7
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2022-02-07 21:12:51 +0000

    Update test.php

--- test.php
+++ test.php
@@ -3,5 +3,10 @@ $servername = "localhost:3306";
 $username = "root";
 $password = "root";
 
-
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 ?>
