9eb317b5db1a92d157747108fe79f1ed4bd19d1a
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2022-02-08 17:46:17 +0000

    Update test.php

--- test.php
+++ test.php
@@ -10,9 +10,9 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SET PASSWORD = 'INPUT'; RENAME USER username@localhost TO INPUT@localhost;"
+$sql = "SET PASSWORD = 'INPUT'; RENAME USER username@localhost TO INPUT@localhost;";
 
-//$result = $conn->query($sql);
+$result = $conn->query($sql);
 
 $conn-> close();
 ?>
