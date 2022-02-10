5bbe3a5b51a12de8716e041db29142c47ca8905f
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2022-02-10 19:16:15 +0000

    Add files via upload

---
+++ firsttimesetup.php
@@ -0,0 +1,32 @@
+<?php
+    $name = htmlspecialchars($_POST["name"], ENT_QUOTES);
+    $updtFreq = htmlspecialchars($_POST["updatefrequency"], ENT_QUOTES);
+    $updtHour = htmlspecialchars($_POST["updatehour"], ENT_QUOTES);
+    $username = htmlspecialchars($_POST["username"], ENT_QUOTES);
+    $password = hash("sha512", (htmlspecialchars($_POST["password"], ENT_QUOTES).time().rand(100,1000)));
+
+    echo $name."<br>";
+    echo $updtFreq."<br>";
+    echo $updtHour."<br>";
+    echo $username."<br>";
+    echo $password."<br>";
+    echo "<br><br>";
+
+    $servername = "localhost:3306";
+    $username = "username";
+    $password = "password";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+
+    //$sql = "SET PASSWORD = 'INPUT';";
+    //mysqli_query($conn "SET PASSWORD = 'newpass'";
+    //$result = $conn->query($sql);
+
+                
+    $conn-> close();
+?>
