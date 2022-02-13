3eb24d9f9de62fd6e851704810513620a2b7292f
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2022-02-13 11:14:44 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -68,6 +68,7 @@
                 $conn = new mysqli($servername, $username, $password, $dbname);
                 // Check connection
                 if ($conn->connect_error) {
+                header("Location: errorHandler.php?s=control-center&e=".$conn->connect_error);
                 die("Connection failed: " . $conn->connect_error);
                 }
 
