543e292c8958500ea16de4addcd514222d46b218
Connor Jarrett <69452544+conjardev@users.noreply.github.com>
2022-02-13 09:38:54 +0000

    Add files via upload

--- firsttimesetup.php
+++ firsttimesetup.php
@@ -19,62 +19,13 @@
         die("Not enough value provided<br>Either the program is broken (start an issue on github), or you're not supposed to be here! (naughty naughty)");
     }
 
-    echo $name."<br>";
-    echo $updtFreq."<br>";
-    echo $updtHour."<br>";
-    echo $newUsername."<br>";
-    echo $newPassword."<br>";
-    echo "<br><br>";
-
-    $servername = "localhost:3306";
-    if (!getPass("controller")) {
-        // No password has been set, connect with default info
-        $username = "username";
-        $password = "password";
-    } else {
-        // A password is in the passwords file, we can assume this means
-        // that it has been set already
-        $username = $newUsername;
-        $password = getPass("controller");
-        $dbname = 'robots';
-    }
-
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } else {
-        echo "Connected to db<br>";
-    }
-
-    if (!getPass("controller")) {
-        // The new password has not yet been created, this means we can change to it
-
-        $sql = "SET PASSWORD = '".$newPassword."';";
-        $result = $conn->query($sql);
-        print_r("Attempted to update password with result ".$result."<br>");
-        
-        $sql = "RENAME USER 'username'@'localhost' TO '".$newUsername."'@'localhost';";
-        $result = $conn->query($sql);
-        print_r("Attempted to update username with result ".$result."<br>");
-        
-        
-        echo "<br>Starting pass write";
-        createPassword("controller", $newPassword);
-        echo "<br>Uploaded";
-
-        updateControllerInfo($name, $updtFreq, $updtHour, $newUsername);
-        // header("Location: adopt-success.php?adoptedip=controller");
-    } else {
-        echo "Info already set<br>";
-        $conn-> close();
+    function writeDB($servername, $newUsername, $newPassword, $dbname) {
         $conn = new mysqli($servername, $newUsername, $newPassword);
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         } else {
-            echo "Connected to db under username ".$newUsername;
+        echo "Connected to db under username ".$newUsername;
             // Set up DB
             $sql = "CREATE DATABASE IF NOT EXISTS ".$dbname.";";
             $result = $conn->query($sql);
@@ -135,10 +86,64 @@
                     echo "<br>Error creating table: " . $conn->error;
                 }
 
-              } else {
+            } else {
                 echo "<br>Error creating database: " . $conn->error;
-              }
-        }
+            }
+    }
+    }
+
+    echo $name."<br>";
+    echo $updtFreq."<br>";
+    echo $updtHour."<br>";
+    echo $newUsername."<br>";
+    echo $newPassword."<br>";
+    echo "<br><br>";
+
+    $servername = "localhost:3306";
+    if (!getPass("controller")) {
+        // No password has been set, connect with default info
+        $username = "username";
+        $password = "password";
+    } else {
+        // A password is in the passwords file, we can assume this means
+        // that it has been set already
+        $username = $newUsername;
+        $password = getPass("controller");
+        $dbname = 'robots';
+    }
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Connected to db<br>";
+    }
+
+    if (!getPass("controller")) {
+        // The new password has not yet been created, this means we can change to it
+
+        $sql = "SET PASSWORD = '".$newPassword."';";
+        $result = $conn->query($sql);
+        print_r("Attempted to update password with result ".$result."<br>");
+        
+        $sql = "RENAME USER 'username'@'localhost' TO '".$newUsername."'@'localhost';";
+        $result = $conn->query($sql);
+        print_r("Attempted to update username with result ".$result."<br>");
+        
+        
+        echo "<br>Starting pass write";
+        createPassword("controller", $newPassword);
+        echo "<br>Uploaded";
+
+        updateControllerInfo($name, $updtFreq, $updtHour, $newUsername);
+        writeDB($servername, $newUsername, $newPassword, $dbname);
+        // header("Location: adopt-success.php?adoptedip=controller");
+    } else {
+        echo "Info already set<br>";
+        $conn-> close();
+        writeDB($servername, $newUsername, $newPassword, $dbname);
     }
     
 
